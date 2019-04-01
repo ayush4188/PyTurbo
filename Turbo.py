a37f53b49feb3fa1910cf3fefefc457ef201a0a0
xymp <49109054+xymp@users.noreply.github.com>
2019-04-01 16:35:01 -0500

    Add files via upload

--- Turbo.py
+++ Turbo.py
@@ -1,27 +1,24 @@
 import requests
 import json
-import threading
-import time
 import random
-from multiprocessing import Process
 from tkinter import *
 from tkinter import messagebox
+import sys
 
-
-def info(l):
-    #create temporary strings
-    tempu = str(input('\nusername\n@'))
-    tempp = str(input('password\n$'))
-    tempt = str(input('target\n@'))
-
-    l['username'] = tempu
-    l['password'] = tempp
-    l['target'] = tempt
-    return l
+tryn = 0
+master = Tk()
 
 def turbo():
-    params = {}
-    log = info(params)
+
+    global tryn
+    global master
+    global text1
+    global insert
+    params = {
+        'username': e1.get(),
+        'password': e2.get(),
+        'target': e3.get()
+    }
 
     #sesssion
 
@@ -38,11 +35,11 @@ def turbo():
 
     #login info
     
-    username = log['username']
+    username = params['username']
 
-    password = log['password']
+    password = params['password']
 
-    targets = log['target']
+    targets = params['target']
 
     data2 = {
         'username': username,
@@ -71,67 +68,89 @@ def turbo():
 
 
     if r2.json()['authenticated'] == False:
-        print('Bad Freshie.... Retry login')
+        messagebox.showinfo("Uh oh", "Invalid Account Info Or Accept Sus Request On Instagram")
+        print("$Failed")
         
-        time.sleep(.2)
-        turbo()
     else:
         csrf = r2.cookies.get_dict()['csrftoken']
-        print('Authenticated')
-        time.sleep(.2)
-    turboin = True
-    #start monitoring the username
-    tryn = 0
-    while turboin == True:
-        res = requests.get('https://www.instagram.com/{0}'.format(log['target']))
-        tryn = tryn + 1
-        print('{0}, {1}, {2}\r'.format(tryn, log['target'], res))
-        if res.status_code == 404:
-            print('Tag Available')
+        print('$Authenticated')
+        turboin = True
+        #start monitoring the username
+        hf = {
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/x-www-form-urlencoded',
+            'origin': 'https://www.instagram.com',
+            'referer': 'https://www.instagram.com/accounts/edit/',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+            'x-csrftoken': csrf,
+            'x-instagram-ajax': '1',
+            'x-requested-with': 'XMLHttpRequest',
+            'Connection': 'keep-alive'
+        }
+        
+        df = {
+            'first_name': 'Claimed',
+            'email': '{}xxx@protonmail.com'.format(str(random.randint(11111111, 99999999))),
+            'username': params['target'],
+            'phone_number':'',
+            'gender': '3',
+            'biography':'',
+            'external_url':'',
+            'chaining_enabled': 'on'
+        }
+        urlf = "https://www.instagram.com/accounts/edit/"
+        master.update()
+        res = None
+        s.headers.update(hf)
+        
+        while turboin == True:
+            tryn += 1
+            if tryn % 20 == 0:
+                text1.delete("1.0", END)
+                text1.insert("1.0", tryn)
+                master.update()
 
-            #init url
-              
-            urlf = "https://www.instagram.com/accounts/edit/"
-
-            #post new data
-              
-            hf = {
-                'accept': '*/*',
-                'accept-encoding': 'gzip, deflate, br',
-                'accept-language': 'en-US,en;q=0.9',
-                'content-type': 'application/x-www-form-urlencoded',
-                'origin': 'https://www.instagram.com',
-                'referer': 'https://www.instagram.com/accounts/edit/',
-                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
-                'x-csrftoken': csrf,
-                'x-instagram-ajax': '1',
-                'x-requested-with': 'XMLHttpRequest'
-            }
+            print('Try:{0}, Target:{1}, Code:{2}'.format(tryn, params['target'], res))
             
-            df = {
-                'first_name': 'Too Slow',
-                'email': '{}xxx@protonmail.com'.format(str(random.randint(11111111, 99999999))),
-                'username': log['target'],
-                'phone_number':'',
-                'gender': '3',
-                'biography':'Claimed Lol',
-                'external_url':'',
-                'chaining_enabled': 'on'
-            }
-            #change the acc to the turbo name
-            s.post(urlf, headers=hf, data=df)
-            print('Completed Turbo Killing Thread')
-            messagebox.showinfo("Claimed", "Claimed @{0}".format(log['target']))
-            turboin = False
-            exit()
-            sys.exit()
-
-    
+            try:
+                res = requests.get('https://www.instagram.com/{0}'.format(params['target']))
+                if res.status_code == 404:
+                    s.post(urlf, data=df)
+                    messagebox.showinfo("Dead", "Killed @{0} | {1} tries".format(params['target'], tryn))
+                    print("\nThreads Killed - Close Program")
+                    turboin = False
+                    master.destroy()
+                    exit(0)
+            except:
+                print("\nconnection closed to instagram. Rebooting...\n")
+                turboin = False
+                turbo()
 
 if __name__ == '__main__':
-    print("PyTurbo")
-    print("---------------------------------------")
-    print("")
-    #turbo()
-    t = threading.Thread(target=turbo())
-    t.start()
+    print("$output console")
+    l1 = Label(master, text="Username", bg="black", fg="red")
+    l2 = Label(master, text="Password", bg="black", fg="red")
+    l3 = Label(master, text="Target", bg="black", fg="red")
+    e1 = Entry(master, bg="black", fg="yellow")
+    e2 = Entry(master, bg="black", fg="yellow", show="*")
+    e3 = Entry(master, bg="black", fg="yellow")
+
+    b1 = Button(master, text='Beam', bg="black", fg="#00FF1A", command=turbo)
+
+    text1 = Text(master, height=1, width=15, bg="black", fg="#90FFFC")
+    text1.insert("1.0", tryn)
+    
+    master.title('$$')
+    l1.pack(fill=X,pady=0)
+    e1.pack(fill=X,pady=0)
+    l2.pack(fill=X,pady=0)
+    e2.pack(fill=X,pady=0)
+    l3.pack(fill=X,pady=0)
+    e3.pack(fill=X,pady=0)
+    b1.pack(fill=X,pady=0)
+    text1.pack(fill=X,pady=0)
+    text1.bind('<1>', lambda event: text1.focus_set())
+
+    master.mainloop()
